d5d831390187498ed337867dcc66319c14c51351
Micah Johnston <micah@glowcoil.com>
2020-12-08 18:37:17 -0600

    remove Message api

--- src/window.rs
+++ src/window.rs
@@ -10,9 +10,7 @@ use crate::x11 as platform;
 #[cfg(target_os = "macos")]
 use crate::macos as platform;
 
-
 pub struct AppRunner(pub(crate) platform::AppRunner);
-
 
 impl AppRunner {
     pub fn app_run_blocking(self){
@@ -20,30 +18,15 @@ impl AppRunner {
     }
 }
 
-
-pub struct WindowHandle<H: WindowHandler>(
-    pub(crate) platform::WindowHandle<H>
-);
-
-
-impl <H: WindowHandler>WindowHandle<H> {
-    pub fn try_send_message(
-        &mut self,
-        message: H::Message
-    ) -> Result<(), H::Message> {
-        self.0.try_send_message(message)
-    }
-}
-
+pub struct WindowHandle(pub(crate) platform::WindowHandle);
 
 pub struct Window<'a>(pub(crate) &'a mut platform::Window);
 
-
-impl <'a>Window<'a> {
+impl<'a> Window<'a> {
     pub fn open<H, B>(
         options: WindowOpenOptions,
         build: B
-    ) -> (WindowHandle<H>, Option<AppRunner>)
+    ) -> (WindowHandle, Option<AppRunner>)
         where H: WindowHandler,
               B: FnOnce(&mut Window) -> H,
               B: Send + 'static
@@ -52,13 +35,11 @@ impl <'a>Window<'a> {
     }
 }
 
-
-unsafe impl <'a>HasRawWindowHandle for Window<'a> {
+unsafe impl<'a> HasRawWindowHandle for Window<'a> {
     fn raw_window_handle(&self) -> RawWindowHandle {
         self.0.raw_window_handle()
     }
 }
-
 
 // Compile-time API assertions
 #[doc(hidden)]
@@ -71,14 +52,11 @@ mod assertions {
     }
 
     impl WindowHandler for TestWindowHandler {
-        type Message = ();
-
         fn on_event(&mut self, _: &mut Window, _: Event) {}
-        fn on_message(&mut self, _: &mut Window, _: Self::Message) {}
         fn on_frame(&mut self) {}
     }
 
     // Assert that WindowHandle is Send even if WindowHandler isn't
     static_assertions::assert_not_impl_any!(TestWindowHandler: Send);
-    static_assertions::assert_impl_all!(WindowHandle<TestWindowHandler>: Send);
+    static_assertions::assert_impl_all!(WindowHandle: Send);
 }
