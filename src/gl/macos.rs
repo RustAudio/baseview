422db5bf79115bfe9c480efc8eda61c758aa1e89
max <max.huttunen@gmail.com>
2025-04-12 11:22:19 +0300

    allow "unexpected_cfgs" in macOS OpenGL code

--- src/gl/macos.rs
+++ src/gl/macos.rs
@@ -1,3 +1,7 @@
+// This is required because the objc crate is causing a lot of warnings: https://github.com/SSheldon/rust-objc/issues/125
+// Eventually we should migrate to the objc2 crate and remove this.
+#![allow(unexpected_cfgs)]
+
 use std::ffi::c_void;
 use std::str::FromStr;
 
