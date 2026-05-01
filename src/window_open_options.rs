1ec490c99fa2bef0c3c81c4f42d0b7552412c50d
Billy Messenger <60663878+BillyDM@users.noreply.github.com>
2026-05-01 01:47:21 -0500

    Add default methods to WindowOpenOptions (#224)

--- src/window_open_options.rs
+++ src/window_open_options.rs
@@ -1,19 +1,24 @@
 use crate::Size;
 
+#[cfg(feature = "opengl")]
+use crate::gl::GlConfig;
+
 /// The dpi scaling policy of the window
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Default, Debug, Clone, Copy, PartialEq)]
 pub enum WindowScalePolicy {
     /// Use the system's dpi scale factor
+    #[default]
     SystemScaleFactor,
     /// Use the given dpi scale factor (e.g. `1.0` = 96 dpi)
     ScaleFactor(f64),
 }
 
 /// The options for opening a new window
+#[derive(Debug, Clone, PartialEq)]
 pub struct WindowOpenOptions {
     pub title: String,
 
-    /// The logical size of the window.
+    /// The logical size of the window
     ///
     /// These dimensions will be scaled by the scaling policy specified in `scale`. Mouse
     /// position will be passed back as logical coordinates.
@@ -24,6 +29,20 @@ pub struct WindowOpenOptions {
 
     /// If provided, then an OpenGL context will be created for this window. You'll be able to
     /// access this context through [crate::Window::gl_context].
+    ///
+    /// By default this is set to `None`.
     #[cfg(feature = "opengl")]
-    pub gl_config: Option<crate::gl::GlConfig>,
+    pub gl_config: Option<GlConfig>,
+}
+
+impl Default for WindowOpenOptions {
+    fn default() -> Self {
+        Self {
+            title: String::from("baseview window"),
+            size: Size { width: 500.0, height: 400.0 },
+            scale: WindowScalePolicy::default(),
+            #[cfg(feature = "opengl")]
+            gl_config: None,
+        }
+    }
 }
