fb40f3c0def5db369acea02f1f9c597496078845
Adrien Prokopowicz <6529475+prokopyl@users.noreply.github.com>
2026-07-29 00:22:29 +0200

    docs

--- src/handler.rs
+++ src/handler.rs
@@ -9,8 +9,16 @@ pub trait WindowHandler: 'static {
     fn on_frame(&self) -> core::result::Result<(), HandlerError>;
     /// Informs the handler that the window has been resized.
     ///
-    /// If this returns an error, the resize operation will be reverted in order to keep the current
-    /// size.
+    /// # Errors
+    ///
+    /// This operation can fail, in which case an [`HandlerError`] can be returned.
+    /// This can happen if e.g. an underlying buffer could not be resized, or some kind of driver error.
+    ///
+    /// In case this `resized` operation fails, `baseview` will assume that it did not meaningfully
+    /// change anything, and that the window is still able to render and operate at the previous size.
+    ///
+    /// It will also attempt to resize the underlying platform window and parent window back to the
+    /// previous size, but this is only a best-effort attempt since those operations can also fail.
     fn resized(&self, new_size: WindowSize) -> core::result::Result<(), HandlerError>;
     fn on_event(&self, event: Event) -> EventStatus;
 }
