724bb6d6e3be17388940dc7c1a120ea7aa3aba5b
Adrien Prokopowicz <6529475+prokopyl@users.noreply.github.com>
2026-08-22 12:05:10 +0200

    wip

---
+++ src/wrappers/egl/sys.rs
@@ -0,0 +1,66 @@
+#![allow(non_snake_case, non_camel_case_types, reason = "To match EGL function naming")]
+
+use crate::platform::gl::CreationFailedError;
+use libloading::Library;
+use std::ffi::*;
+use std::fmt::{Display, Formatter};
+
+pub type Enum = c_uint;
+pub type Boolean = c_uint;
+pub type Int = c_int;
+
+pub type eglGetError = unsafe extern "system" fn() -> c_int;
+pub type eglBindAPI = unsafe extern "system" fn(Enum) -> Boolean;
+pub type eglQueryAPI = unsafe extern "system" fn() -> Enum;
+
+pub const NONE: Int = 0x3038;
+pub const ENUM_NONE: Enum = 0x3038;
+pub const OPENGL_API: Enum = 0x30A2;
+pub const FALSE: Boolean = 0;
+
+#[derive(Debug, Copy, Clone)]
+pub struct MissingSymbolError {
+    name: &'static CStr,
+}
+
+impl Display for MissingSymbolError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl From<MissingSymbolError> for CreationFailedError {
+    fn from(value: MissingSymbolError) -> Self {
+        Self::EGLMissingSymbol(value)
+    }
+}
+
+impl From<libloading::Error> for CreationFailedError {
+    fn from(value: libloading::Error) -> Self {
+        Self::EGLLoadError(value)
+    }
+}
+
+pub struct Functions {
+    pub eglGetError: eglGetError,
+    pub eglBindAPI: eglBindAPI,
+    pub eglQueryAPI: eglQueryAPI,
+}
+
+impl Functions {
+    pub unsafe fn load_from(library: &Library) -> Result<Self, CreationFailedError> {
+        Ok(Self {
+            eglGetError: Self::get(library, c"eglGetError")?,
+            eglBindAPI: Self::get(library, c"eglBindAPI")?,
+            eglQueryAPI: Self::get(library, c"eglQueryAPI")?,
+        })
+    }
+
+    unsafe fn get<T: Copy>(
+        library: &Library, name: &'static CStr,
+    ) -> Result<T, CreationFailedError> {
+        let symbol = library.get::<Option<T>>(name)?;
+        let symbol = symbol.lift_option().ok_or(MissingSymbolError { name })?;
+        Ok(*symbol)
+    }
+}
