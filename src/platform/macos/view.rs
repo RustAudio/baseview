4af1bfbe031059fe26501f1602962163c763ba48
Adrien Prokopowicz <6529475+prokopyl@users.noreply.github.com>
2026-08-28 12:01:12 +0200

    Remove unneeded unsafe

--- src/platform/macos/view.rs
+++ src/platform/macos/view.rs
@@ -19,8 +19,8 @@ use objc2::rc::Weak;
 use objc2::runtime::{NSObjectProtocol, ProtocolObject};
 use objc2::{msg_send, AllocAnyThread, ClassType, MainThreadMarker};
 use objc2_app_kit::{
-    NSApplication, NSDragOperation, NSDraggingInfo, NSEvent, NSFilenamesPboardType, NSTrackingArea,
-    NSTrackingAreaOptions, NSView, NSWindow,
+    NSApplication, NSCursor, NSDragOperation, NSDraggingInfo, NSEvent, NSFilenamesPboardType,
+    NSTrackingArea, NSTrackingAreaOptions, NSView, NSWindow,
 };
 use objc2_foundation::{NSArray, NSNotification, NSPoint, NSRect, NSSize, NSString};
 use std::cell::{Cell, RefCell};
@@ -275,9 +275,7 @@ impl Drop for BaseviewView {
     fn drop(&mut self) {
         self.state.closed.set(true);
         if self.state.cursor_hidden.get() {
-            unsafe {
-                let _: () = objc2::msg_send![objc2_app_kit::NSCursor::class(), unhide];
-            }
+            NSCursor::unhide();
         }
     }
 }
